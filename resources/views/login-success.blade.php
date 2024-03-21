dec75862fa3a45de0cb739d9715f115cdac19084
angie <>
2024-03-21 14:42:51 +0800

    update translations

--- resources/views/login-success.blade.php
+++ resources/views/login-success.blade.php
@@ -1,5 +1,5 @@
 <x-filament-panels::page.simple>
     <x-filament::button href="{{ \Filament\Facades\Filament::getUrl() }}" tag="a">
-        @lang('filament-email-2fa::filament-email-2fa.login_success')
+        @lang('filament-email-2fa::filament-email-2fa.continue')
     </x-filament::button>
 </x-filament-panels::page.simple>
