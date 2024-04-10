1b9c86aef123c732f80cc5f24f9dc2017909c763
angie <>
2024-04-10 12:14:04 +0800

    bug

--- src/FilamentEmail2faPlugin.php
+++ src/FilamentEmail2faPlugin.php
@@ -18,7 +18,7 @@ class FilamentEmail2faPlugin implements Plugin
     public function register(Panel $panel): void
     {
         $fapage = config('filament-email-2fa.2fa_page', TwoFactorAuth::class);
-        $login_success_page = config('filament-email-2fa.2fa_page', LoginSuccessPage::class);
+        $login_success_page = config('filament-email-2fa.login_success_page', LoginSuccessPage::class);
 
         $panel->pages([
             $fapage,
